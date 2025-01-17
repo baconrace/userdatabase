d496410616b3c21fbd9df47bf279a6b8d43839b4
baconrace <ricsto@krokeide.vgs.no>
2025-01-17 08:31:00 +0100

    Made preps for login

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const { addUser } = require("./database/services");
+const { addUser, authenticateUser } = require("./database/services");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,6 +21,7 @@ app.get("/signup", (req, res) => {
 app.post("/signup", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+
   addUser(email, password);
 
   return res.redirect("/signup");
@@ -30,13 +31,20 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  addUser(email, password);
+  const auth = authenticateUser(email, password);
 
+  if (await auth) {
+    return res.redirect("/dashboard");
+  }
   return res.redirect("/login");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
 });
 
 app.listen(port, () => {
